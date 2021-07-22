1b0893c94d0a1be34b2c7ea076d670aed135597f
Distant-ideal <42264493+Distant-ideal@users.noreply.github.com>
2021-07-22 21:54:57 +0800

    Add files via upload

---
+++ Sprites/Slash 2/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField]
+    private float moveSpeed;
+    private Transform target;
+    [SerializeField]
+    private float maxHp;
+
+    public float hp;
+
+    [Header("Hurt")]
+    private SpriteRenderer sp;
+    public float hurtLength; //持续时间
+    private float hurtCounter; //计数器
+
+    [HideInInspector]
+    public bool IsAttacked;
+    public GameObject ExplosionEffect;
+
+
+    private void Start()
+    {
+        hp = maxHp;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        FllowPlayer();
+        if(hurtCounter <= 0)
+        {
+            sp.material.SetFloat("_FlashAmount", 0);
+        } else
+        {
+            hurtCounter -= Time.deltaTime;
+        }
+    }
+
+    private void FllowPlayer()
+    {
+        //敌人当前坐标 目标位置坐标信息 最大距离 = 速度 * 时间
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime); 
+    }
+
+    public void TakenDamage(float _amount)
+    {
+        IsAttacked = true;
+        hp -= _amount;
+        StartCoroutine(IsAttack()); //使用协程进行对攻击判定的检测，保证只进行一次攻击
+        HurtShaker();
+        if (hp <= 0)
+        {
+            Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void HurtShaker()
+    {
+        sp.material.SetFloat("_FlashAmount", 1);
+        hurtCounter = hurtLength;
+    }
+
+    IEnumerator IsAttack()
+    {
+        yield return new WaitForSeconds(0.2f);
+        IsAttacked = false;
+    }
+}
---
+++ Sprites/Slash 2/Scripts/PlayerAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour 
+{
+	private void Update () 
+	{
+        if (Input.GetMouseButtonDown(0))
+        {
+			Attack();
+			StartCoroutine(FindObjectOfType<CameraContrller>().CameraShake(0.4f, 0.4f));
+		}
+	}
+
+	private void Attack()
+    {
+		//鼠标位置【目标点位置】-当前位置【人物所在位置】
+		Vector2 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;// Radius -> Degree弧度转角度
+		transform.rotation = Quaternion.Euler(0, 0, rotZ);
+		transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
